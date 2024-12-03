57da1a3e3b80c8a6f50cadf3e0a7a38500c6f899
EmirioBomb <wjhlfamily@gmail.com>
2024-12-03 16:39:12 +0800

    fix(configuration): 增加日志相关配置

--- configuration/src/main/java/com/emirio/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/com/emirio/configuration/ConfigurationApplication.java
@@ -10,10 +10,10 @@ public class ConfigurationApplication {
 
     public static void main(String[] args) {
         // SpringBoot未初始化前的日志输出
-        log.warn("正在准备启动SpringBootTutorial程序...");
+        log.warn("正在准备启动: SpringBootTutorial");
         SpringApplication.run(ConfigurationApplication.class, args);
         // SpringBoot启动后的日志输出
-        log.warn("成功启动SpringBootTutorial程序...");
+        log.warn("成功启动: SpringBootTutorial...");
     }
 
 }
