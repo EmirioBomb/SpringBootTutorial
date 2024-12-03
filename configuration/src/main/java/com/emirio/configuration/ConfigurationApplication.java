1c31582e9cc4a596ac138b2581da5bd984203817
EmirioBomb <wjhlfamily@gmail.com>
2024-12-03 13:37:23 +0800

    feature(configuration): 新增日志支持

--- configuration/src/main/java/com/emirio/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/com/emirio/configuration/ConfigurationApplication.java
@@ -1,13 +1,19 @@
 package com.emirio.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ConfigurationApplication {
 
     public static void main(String[] args) {
+        // SpringBoot未初始化前的日志输出
+        log.warn("正在准备启动SpringBootTutorial程序...");
         SpringApplication.run(ConfigurationApplication.class, args);
+        // SpringBoot启动后的日志输出
+        log.warn("成功启动SpringBootTutorial程序...");
     }
 
 }
