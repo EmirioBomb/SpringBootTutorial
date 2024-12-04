adc76c3ce535eb4f7a79d1b79579cb7c33a55d5b
EmirioBomb <wjhlfamily@gmail.com>
2024-12-04 17:15:37 +0800

    feature(Runner): CommandLineRunner 与 ApplicationRunner 的高级用法

--- configuration/src/main/java/com/emirio/configuration/runner/LogAppInfoAppRunner.java
+++ configuration/src/main/java/com/emirio/configuration/runner/LogAppInfoAppRunner.java
@@ -3,14 +3,21 @@ package com.emirio.configuration.runner;
 import com.emirio.configuration.domain.properties.AppConfigProperties;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Slf4j
 @Component
 @Setter
 public class LogAppInfoAppRunner implements ApplicationRunner {
+    @Value("${args}")
+    private String[] args;  // 模拟系统参数列表
+
     private AppConfigProperties appConfigProperties;
 
     public LogAppInfoAppRunner(AppConfigProperties appConfigProperties) {
@@ -20,5 +27,13 @@ public class LogAppInfoAppRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("成功启动系统AppRunner: {}", appConfigProperties.getName());
+        // 此处为模拟启动参数
+        // 在 Lambda 表达式中，无法直接使用非 final 类型的局部变量（如 args），这是因为 Lambda 捕获外部变量时要求它们是 final 或者等效于 final
+        final ApplicationArguments appArgs = new DefaultApplicationArguments(this.args);
+        log.info("系统启动AppRunner参数列表为: {}", Arrays.toString(appArgs.getSourceArgs()));
+        // 遍历选项参数
+        log.info("系统启动的非选项参数列表为: {}", appArgs.getNonOptionArgs());
+        log.info("系统启动的选项参数列表为: {}", appArgs.getOptionNames());
+        appArgs.getOptionNames().forEach(key -> log.info("选项参数: {}, 值: {}", key, appArgs.getOptionValues(key)));
     }
 }
--- configuration/src/main/java/com/emirio/configuration/runner/LogAppInfoCmdRunner.java
+++ configuration/src/main/java/com/emirio/configuration/runner/LogAppInfoCmdRunner.java
@@ -2,8 +2,11 @@ package com.emirio.configuration.runner;
 
 import com.emirio.configuration.domain.properties.AppConfigProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 /**
  * SpringBoot 启动后执行初始化操作
@@ -11,6 +14,10 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class LogAppInfoCmdRunner implements CommandLineRunner {
+    // 初始化启动参数样例
+    @Value("${args}")
+    private String[] args;
+
     private final AppConfigProperties appConfigProperties;
 
     public LogAppInfoCmdRunner(AppConfigProperties appConfigProperties) {
@@ -20,5 +27,6 @@ public class LogAppInfoCmdRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("成功启动系统CmdRunner: {}", appConfigProperties.getName());
+        log.info("系统启动CmdRunner参数为: {}", Arrays.toString(this.args));
     }
 }
